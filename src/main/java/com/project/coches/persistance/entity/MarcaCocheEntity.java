ae50f061b3f5459218aabfec224b0b392a56d47a
Scataneo <sergio.cataneo@gmail.com>
2023-09-23 23:03:00 -0300

    Get and Set on MarcaCocheEntity

--- src/main/java/com/project/coches/persistance/entity/MarcaCocheEntity.java
+++ src/main/java/com/project/coches/persistance/entity/MarcaCocheEntity.java
@@ -1,12 +1,15 @@
 package com.project.coches.persistance.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 
 /*
  *Entidades de marca coche
  */
 
+@Getter @Setter
 @Entity
 @Table(name = "marca_coche")
 public class MarcaCocheEntity {
